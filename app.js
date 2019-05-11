956eec0abf31d2626598823bf5e910cd8b7579c2
Joëlle Maximin <joelle.maximin@gmail.com>
2019-05-11 13:31:06 +0200

    Add Index, App.js, CSS and Pictures Files

---
+++ app.js
@@ -0,0 +1,11 @@
+const bounceDown = document.getElementById("animate")
+
+  bounceDown.addClass('animated bounceInDown');
+}
+
+setTimeout(doubleDown, 2000)
+
+$('#left-in').addClass('animated fadeInLeft');
+
+$('#right-in').addClass('animated fadeInRight');
+
