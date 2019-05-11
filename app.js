e2c756ebe57c573ac6509c69b47d0ab87e56868c
Joëlle Maximin <joelle.maximin@gmail.com>
2019-05-11 16:03:48 +0200

    updating css file and add some links

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const bounceDown = document.getElementById("animate")
+const bounceDown = document.getElementById("animate"){
 
   bounceDown.addClass('animated bounceInDown');
+
 }
 
 setTimeout(doubleDown, 2000)
@@ -9,3 +10,10 @@ $('#left-in').addClass('animated fadeInLeft');
 
 $('#right-in').addClass('animated fadeInRight');
 
+function linkMe() {
+  document.getElementById("kanna").innerHTML = "https://kanasukre-location.herokuapp.com/en/";
+  document.getElementById("garden").innerHTML = "https://github.com/joellemaximin/gardenmarket";
+  document.getElementById("restau").innerHTML = "https://joellemaximin.github.io/restaurantwebsite/";
+  document.getElementById("car-store").innerHTML = "https://joellemaximin.github.io/car-store/";
+
+}
